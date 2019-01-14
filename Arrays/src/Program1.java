3f2a79497b499b998294ba0e8c425149427bb36d
prernasingal <prernasingal@ncuindia.edu>
2019-01-14 11:53:40 +0530

    Arrays Program 1

--- Arrays/src/Program1.java
+++ Arrays/src/Program1.java
@@ -1,65 +1,41 @@
-/*Program 20: Problem statement
-=================================
-Use a one-dimensional array to solve the following problem: Write an application that inputs five numbers, each between 10 and 100, inclusive. As each number is read, display it only if it's not a duplicate of a number already read. Provide for the "worst case," in which all five numbers are different. Display the complete set of unique values input after the user enters each new value. a) Without using function b) Using function.
-=================================
-Definition of Done:
-a) The program should ask the user to enter 5 numbers.
-b) The program should generate an error message is the value is not between 10 and 100 and ask the user to re-enter.
-c) The program should display the non-duplicate entries only after each new value is read.
-=================================*/
-import java.util.Scanner;
-class Program1 {
-    static int[] arr;               	//Array declaration
-       public static void main(String[] args) {
-           arr = new int[5];           //Array definition
-           inputnum();                 //Number input
-       
-    }
-    static boolean isduplicate(int pos, int n) {    //Fuinction to check duplicate
-            for (int k=pos-1;k>=0;k--) {            
-                if(n==arr[k]){                      //Compare already filled array elements
-                    return false;
-                }
+import java.util.*;
+class Program1{
+    public static void main(String args[]){
+        int num[] = new int[10];
+        int rowcounter;
+        int searchElement;
+        boolean flag;
+        Scanner in = new Scanner(System.in);
+        //input from user
+        for(rowcounter=0;rowcounter<num.length;rowcounter++){
+            System.out.print("Enter the element [" + (rowcounter+1) + "] : ");
+            num[rowcounter] = in.nextInt();
+            System.out.println();
+        }
+
+        //Ask for element to be searched
+        System.out.print("Enter the Element to be searched : ");
+        searchElement = in.nextInt();
+
+        //print the array
+        System.out.println("Elements in the array are : ");
+        for(rowcounter=0;rowcounter<num.length;rowcounter++){
+            System.out.println(num[rowcounter]);
+        }
+
+        //search for the element
+        flag = false;
+        for(rowcounter=0;rowcounter<num.length;rowcounter++){
+            if(num[rowcounter] == searchElement){
+                flag = true;
+                break;
             }
-            return true;
-           }
-           static void inputnum() {                //Input number
-               Scanner num = new Scanner(System.in);
-           
-               boolean c;
-               int n,i;
-               for(i=0;i<5;i++){
-                   System.out.println(i+"Enter number:");
-                   n = num.nextInt();
-                   while(!checkbetween(n)) {
-                       System.out.println("Enter integer between 10 - 100:");  //check validity
-                       n = num.nextInt();
-                   }
-                   if(!isduplicate(i,n)){          //check duplicate
-                     System.out.println("Duplicate entry");
-                       i=i-1;
-                       continue;
-                   }
-                   arr[i]=n;
-		   printarray(i);
-               }
-           }
-           static boolean checkbetween(int n) {    //function to check number inbetween 10-100
-               if(n>9 && n<101){
-                   return true;
-               }
-               else {
-                   return false;
-               }
-                   
-           }
-	   static void printarray(int pos){
-		   System.out.println("=================");	
-	           for(int i=0;i<=pos;i++){       //Array printing
-	               System.out.print(arr[i]+" ");
-        	   }
-		   System.out.println("\n=================");	
-	   }	
-
+        }
+        if(flag){
+            System.out.println("Element found at location : " + rowcounter);
+        }
+        else{
+            System.out.println("-1");
+        }
+    }
 }
-
